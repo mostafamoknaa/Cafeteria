375176875dd8d07c57c0b45572cd4c38e616e2a0
Mostafa <mostafamokna78@gmail.com>
2025-04-28 22:08:53 +0300

    add image to user

--- shared/handle_registration.php
+++ shared/handle_registration.php
@@ -11,7 +11,35 @@ if (isset($_POST["btn"])) {
     $password = $_POST["password"];
     $confirm_password = $_POST["confirm_password"];
     // $role = $_POST["role"];
-    $picture_name = '';
+    $picture = '';
+    if (empty($error) && isset($_FILES["picture"])) {
+        $targetDir = "../images/";
+        $fileExt = strtolower(pathinfo($_FILES["picture"]["name"], PATHINFO_EXTENSION));
+        $fileName = time() . '_' . uniqid() . '.' . $fileExt;
+        $targetFile = $targetDir . $fileName;
+
+
+        $check = getimagesize($_FILES["picture"]["tmp_name"]);
+        if ($check === false) {
+            $error = "File is not a valid image!";
+        } elseif ($_FILES["picture"]["size"] > 5000000) {
+            $error = "Image size too large (max 5MB)!";
+        }
+
+        $allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
+        if (!in_array($fileExt, $allowedTypes)) {
+            $error = "Only JPG, JPEG, PNG & GIF files are allowed!";
+        }
+
+        if (empty($error)) {
+            if (!move_uploaded_file($_FILES["picture"]["tmp_name"], $targetFile)) {
+                $error = "Error uploading image!";
+            } else {
+                $picture = $targetFile;
+            }
+        }
+    }
+
 
     if ($password !== $confirm_password) {
         $errors['confirm_password'] = "Passwords do not match";
@@ -34,7 +62,7 @@ if (isset($_POST["btn"])) {
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     $sql = "INSERT INTO users (name, email, password, picture) 
-            VALUES ('$name', '$email', '$hashed_password', '$picture_name')";
+            VALUES ('$name', '$email', '$hashed_password', '$picture')";
 
     if (mysqli_query($conn, $sql)) {
         $_SESSION['success'] = "Registration successful! You can now log in.";
